5707aae8cba22ada43e0ab2c30e19f3691dc61f0
GetRektByMe <byronvanstien@rocketmail.com>
2016-11-09 00:50:06 +0000

    add gitignore and main.js contents

---
+++ main.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require("electron")
+
+let mainApp
+
+function createWindow() {
+    let mainApp = new BrowserWindow({
+        width: 1280,
+        height: 720
+    })
+
+    return mainApp;
+}
+
+app.on("ready", createWindow)
