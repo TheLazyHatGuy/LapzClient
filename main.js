54e7b5fab0be5120e0bb861a313e1db590ac519a
cscholes982 <cscholes982@gmail.com>
2016-11-15 07:40:32 +0000

    Fixed Main.js conflict

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-const {app, BrowserWindow} = require("electron");
-//require(__dirname + "/app/static/js/components.js");
-=======
 import { app, BrowserWindow } from "electron";
->>>>>>> refs/remotes/GetRektByMe/dev
 
 // Global reference so it doesn't get GC'd
 var win;
